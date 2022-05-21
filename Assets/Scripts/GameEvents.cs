ff760d2bb69f93133e13a714de80acf4fd2c5c3b
gui45 <guillaume.d.marceau@gmail.com>
2022-05-20 21:37:14 -0400

    Set base inputs

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameEvents instance { get; private set; }
+
+    public event Action<Vector2> onLookEvent;
+    public event Action<float> onMoveEvent;
+    public event Action onJumpEvent;
+
+    void Awake()
     {
-        
+        if (instance != null)
+        {
+            Debug.LogError("Game Event instance is already set");
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnJump()
     {
-        
+        //Debug.Log("jump");
+        onJumpEvent?.Invoke();
+    }
+
+    public void OnMove(InputValue input)
+    {
+        //Debug.Log("Move " + input.Get<float>());
+        onMoveEvent?.Invoke(input.Get<float>());
+    }
+
+    public void OnLook(InputValue input)
+    {
+        //Debug.Log("Look " + input.Get<Vector2>());
+        onLookEvent?.Invoke(input.Get<Vector2>());
     }
 }
